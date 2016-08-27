247893879e5611ad18bb260dd2e1c4905bf9a5e6
Andrey Antukh <niwi@niwi.nz>
2016-08-27 15:41:20 +0300

    Set version to 1.0.1.

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,12 @@
 # Changelog #
 
+## Version 1.0.1 ##
+
+Date: 2016-08-26
+
+- Minor internal change.
+
+
 ## Version 1.0.0 ##
 
 Date: 2016-08-26
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/bide "1.0.0"
+(defproject funcool/bide "1.0.1"
   :description "Simple routing for ClojureScript"
   :url "https://github.com/funcool/bide"
   :license {:name "BSD (2-Clause)"
