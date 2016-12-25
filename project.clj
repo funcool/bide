741edf0e547bb7865373b63ad5c63254c594424e
Andrey Antukh <niwi@niwi.nz>
2016-12-25 11:44:04 +0100

    Set version to 1.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/bide "1.2.1"
+(defproject funcool/bide "1.2.2"
   :description "Simple routing for ClojureScript"
   :url "https://github.com/funcool/bide"
   :license {:name "BSD (2-Clause)"
