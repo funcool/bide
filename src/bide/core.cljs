9265606e18fb95d76e1234cc04d6ddc55a1e7649
Andrey Antukh <niwi@niwi.nz>
2016-12-29 22:36:44 +0100

    Update copyright year on core ns.

--- src/bide/core.cljs
+++ src/bide/core.cljs
@@ -1,4 +1,4 @@
-;; Copyright (c) 2016 Andrey Antukh <niwi@niwi.nz>
+;; Copyright (c) 2016-2017 Andrey Antukh <niwi@niwi.nz>
 ;; All rights reserved.
 ;;
 ;; Redistribution and use in source and binary forms, with or without
