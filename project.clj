d0d53f60830857f15fa04a83ef14a4ae7b996599
Paul Anderson <paul@andersonpaul.com>
2018-07-31 20:36:53 -0500

    Update version to 1.6.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/bide "1.6.0"
+(defproject funcool/bide "1.6.1-SNAPSHOT"
   :description "Simple routing for ClojureScript"
   :url "https://github.com/funcool/bide"
   :license {:name "BSD (2-Clause)"
